308a71fd234d9776ecbce652ca81a5a59a08b012
Fredrik Sjöholm <fredrik.sjoholm@soderbergpartners.se>
2017-02-13 09:17:26 +0100

    First test ok and added actual FileSystemHandler

--- MiniGit.Domain/Commiter.cs
+++ MiniGit.Domain/Commiter.cs
@@ -4,12 +4,18 @@ namespace MiniGit.Domain
 {
     public class Commiter
     {
+        private readonly IFileSystemHandler _fileSystemHandler;
+
         public Commiter(IFileSystemHandler fileSystemHandler)
-        {            
+        {
+            _fileSystemHandler = fileSystemHandler;
         }
 
         public void Commit(MemoryStream memoryStream)
         {
+            var sha1Generator = new Sha1Generator();
+            var hash = sha1Generator.GenerateHash(memoryStream);
+            _fileSystemHandler.CreateDirectory(hash.Substring(0, 2));
         }
     }
 }

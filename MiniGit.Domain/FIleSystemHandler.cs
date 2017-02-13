135b827eec46fe2814f9fdf32c7d5966c4ad44d7
Fredrik Sjöholm <fredrik.sjoholm@soderbergpartners.se>
2017-02-13 09:20:15 +0100

    First test ok and added actual FileSystemHandler

--- MiniGit.Domain/FIleSystemHandler.cs
+++ MiniGit.Domain/FIleSystemHandler.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MiniGit.Domain
 {
-    class FIleSystemHandler
+    public class FileSystemHandler : IFileSystemHandler
     {
+        public void CreateDirectory(string name)
+        {
+            if (!Directory.Exists(name))
+                Directory.CreateDirectory(name);
+        }
+
+        public void CreateZippedFile(string relPath, string name, Stream content)
+        {
+            throw new NotImplementedException();
+        }
     }
 }

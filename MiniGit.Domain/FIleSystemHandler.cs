e70202dea1ec371343ed36f45d35acef12ec5d57
Fredrik Sjöholm <fredrik.sjoholm@soderbergpartners.se>
2017-02-14 12:41:00 +0100

    Implemented FileSystemHandler

--- MiniGit.Domain/FIleSystemHandler.cs
+++ MiniGit.Domain/FIleSystemHandler.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,7 +18,39 @@ namespace MiniGit.Domain
 
         public void CreateZippedFile(string relPath, string name, Stream content)
         {
-            throw new NotImplementedException();
+            using (FileStream compressedFile = File.Create(name))
+            {
+                using (GZipStream compressionStream = new GZipStream(compressedFile, CompressionMode.Compress))
+                {
+                    content.CopyTo(compressionStream);
+                }
+            }
+        }
+
+        public Stream UnzipFile(string name)
+        {
+            var memoryStream = new MemoryStream();
+            using (FileStream compressedFileStream = File.OpenRead(name))
+            {                
+                using (GZipStream decompressionStream = new GZipStream(compressedFileStream, CompressionMode.Decompress))
+                {
+                    decompressionStream.CopyTo(memoryStream);
+                }
+            }
+            memoryStream.Position = 0;
+            return memoryStream;
+        }
+
+        public void SaveStreamToFile(string path, string filename, Stream content)
+        {
+            var filestream = File.OpenWrite(filename);
+            content.CopyTo(filestream);
+            filestream.Flush();
+        }
+
+        public Stream GetStreamFromFile(string filename)
+        {
+            return File.OpenRead(filename);
         }
     }
 }
--- MiniGit.Domain/IFileSystemHandler.cs
+++ MiniGit.Domain/IFileSystemHandler.cs
@@ -11,5 +11,8 @@ namespace MiniGit.Domain
     {
         void CreateDirectory(string name);
         void CreateZippedFile(string relPath, string name, Stream content);
+        Stream UnzipFile(string name);
+        void SaveStreamToFile(string path, string filename, Stream content);
+        Stream GetStreamFromFile(string filename);
     }
 }
